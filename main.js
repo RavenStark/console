b51943a7d8a03de686aac2da1968c85a765c8355
RavenStark <107474741+RavenStark@users.noreply.github.com>
2022-10-10 14:16:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var firstName = "Snigdho";
+console.log(firstName); 
+var lastName= "Banerjee";
+console.log(lastName);
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+
+
+
+var a = 70;
+var b = 7;
+console.log(a);
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var subtract = a - b;
+console.log(subtract);
+
+var multiply = a * b;
+console.log(multiply);
+
+var divide = a / b;
+console.log(divide);
